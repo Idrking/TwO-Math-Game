861b40791faa216c99a647b25e65978848d4ba04
Isaac King <idrkings@gmail.com>
2021-03-01 21:02:47 +0000

    Implement question class

---
+++ classes/question.rb
@@ -0,0 +1,23 @@
+class Question
+
+  def initialize
+    @val_one = rand(1..20)
+    @val_two = rand(1..20)
+    @answer = @val_one + @val_two
+  end
+  attr_accessor :val_one, :val_two, :answer
+  
+  def question_string
+    "What does #{val_one} plus #{val_two} equal?"
+  end
+
+  def correct?(a)
+    a == self.answer
+  end
+end
+
+
+q = Question.new
+p q
+
+p q.correct?(12)
--- game.rb
+++ game.rb
@@ -1,19 +1,24 @@
 require './classes/player'
+require './classes/question'
 
 class Game
   
   def initialize
   @players = [Player.new(1), Player.new(2)]
   @isOver = false
-  @currentTurn = nil
+  @currentTurn = 0
   end
 
   attr_accessor :players, :isOver, :currentTurn
 
   def start
     
-    while @players[0].lives != 0 && @players[1].lives != 0
-      ##ask current a question
+    while players[0].lives != 0 && players[1].lives != 0
+      question = Question.new
+      puts "Player #{currentTurn + 1}: #{question.question_string}"
+      unless question.correct?(gets.chomp.to_i)
+      
+      end
       ## if they get it wrong, subtract one life
       ## advance the turn
 
